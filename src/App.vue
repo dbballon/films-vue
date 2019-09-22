9d55fff8acfa96f30eb566d6db9d5aa618dc22a4
Dov Ber Ballon <dbballon+git@gmail.com>
2019-09-22 16:23:34 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Films />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Films from './components/Films'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Films
   }
 }
 </script>
