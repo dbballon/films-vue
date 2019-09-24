bbd10314f4681243b05586e694ca99c2eab29a04
Dov Ber Ballon <dbballon+git@gmail.com>
2019-09-24 19:04:41 +0300

    background from app to body

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,14 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background : url(https://cdn.hipwallpaper.com/i/93/54/GyZOrS.jpg);
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  background : url(https://cdn.hipwallpaper.com/i/93/54/GyZOrS.jpg);
   &> img {
     width: 50px;
     margin: 15px;
